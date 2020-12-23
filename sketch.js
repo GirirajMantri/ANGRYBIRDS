fbcbc41e3827b7b1844418981912d8a205a9d14c
GirirajMantri <girirajmantri2006@gmail.com>
2020-12-23 19:35:59 +0530

    added plateform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
   world = engine.world;
 
   ground = new Ground(600, height, 1200, 20);
-
+  plateform = new Ground(150, 300, 300, 170);
   box1 = new Box(700, 320, 70, 70);
   box2 = new Box(920, 320, 70, 70);
   pig1 = new Pig(810, 350);
@@ -56,4 +56,5 @@ function draw() {
   log5.display();
 
   bird.display();
+  plateform.display();
 }
